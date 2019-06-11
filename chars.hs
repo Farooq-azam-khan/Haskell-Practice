4e9e6f0719884f5b3b6262ec0ffc86847722a3ae
Farooq Khan <33574913+Farooq-azam-khan@users.noreply.github.com>
2019-06-11 16:38:45 -0400

    Create chars.hs

---
+++ chars.hs
@@ -0,0 +1,10 @@
+import Data.Char
+
+main = do 
+  putStrLn $ show $ isSpace ' '
+  putStrLn $ show $ isLower 'a'
+  putStrLn $ show $ isUpper 'A'
+  putStrLn $ show $ isAlpha '1'
+  putStrLn $ show $ isAlphaNum ')'
+  putStrLn $ show $ toUpper 'a'
+  putStrLn $ show $ ord 'a'
